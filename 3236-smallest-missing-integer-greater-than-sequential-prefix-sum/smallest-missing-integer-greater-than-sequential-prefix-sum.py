0089280e62bd9edb3b91f07e5372973e4531c1b0
KEERTHI VARDHAN AGARWAL <2400031539@kluniversity.in>
2026-08-11 10:53:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (51.28%) - LeetSync

---
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def missingInteger(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        s=nums[0]
+        for i in range(1,len(nums)):
+            if nums[i]==nums[i-1]+1:
+                s+=nums[i]
+            else:
+                break
+        while s in nums:
+            s+=1
+        return s
+
