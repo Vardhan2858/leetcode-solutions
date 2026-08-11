3bb80c82c72a606361f8701dc6ffaaca2a26526f
KEERTHI VARDHAN AGARWAL <2400031539@kluniversity.in>
2026-08-11 10:11:11 +0530

    Time: 3 ms (54.20%) | Memory: 13.2 MB (36.26%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        seen={}
+        for i in range(len(nums)):
+            c=target-nums[i]
+            if c in seen:
+                return [seen[c],i]
+            seen[nums[i]]=i
