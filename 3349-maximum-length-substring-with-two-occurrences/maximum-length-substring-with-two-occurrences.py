2275fb1526ab2d2d5e4fca2a83ba516333932250
KEERTHI VARDHAN AGARWAL <2400031539@kluniversity.in>
2026-08-14 14:04:43 +0530

    Time: 6 ms (62.86%) | Memory: 12.3 MB (56.43%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.py
@@ -4,19 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        freq = {}
-        left = 0
-        ans = 0
-
+        freq={}
+        left=0
+        ans=0
         for right in range(len(s)):
-            freq[s[right]] = freq.get(s[right], 0) + 1
-
-            while freq[s[right]] > 2:
-                freq[s[left]] -= 1
-                left += 1
-
-            ans = max(ans, right - left + 1)
-
+            freq[s[right]]=freq.get(s[right],0)+1
+            while freq[s[right]]>2:
+                freq[s[left]]-=1
+                left+=1
+            ans=max(ans,right-left+1)
         return ans
-
-        
